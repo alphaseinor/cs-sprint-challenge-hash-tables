e5adc65d08203d1f75ebd078e7f0a3bacdcec737
Brian Hague <alphaseinor@gmail.com>
2020-09-11 21:06:04 -0500

    ex1 is finished

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,17 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+
+    if len(weights) < 2:
+        return None
+
+    index = 0
+
+    for weight in weights:
+        if limit - weight in cache:
+            return(index, cache[limit - weight])
+        cache[weight] = index
+        index += 1
 
     return None
