867b97430d8f844664bf648413ca7dd99e3c0f7b
Brian Hague <alphaseinor@gmail.com>
2020-09-11 21:54:23 -0500

    holy bat edge cases batman! you forgot to account for 0 not being a number!

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -8,7 +8,7 @@ def has_negatives(a):
     result = []
 
     for number in a:
-        if number not in cache:
+        if number not in cache and number != 0: #serously... 0? come on! it's positive, it's negative, it's an empty value, just like my heart... what an edge case!!!
             cache[number] = number
             if -number in cache: #didn't know -number could exist until this day... very cool!
                 result.append(abs(number)) #well... needs to be positive, so abs get a good workout
